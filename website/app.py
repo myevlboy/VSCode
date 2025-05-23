9c92fb2f358af952c284ab7ea7cd704084b40174
myevlboy <gusadornettos@yahoo.com>
2025-05-22 20:24:23 -0500

    got rid of files to start over

--- website/app.py
+++ website/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.column(db.String(200), nullable=False)
+    dat
 
 @app.route('/')
 def hello_world():
